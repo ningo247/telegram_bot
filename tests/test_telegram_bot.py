69830e637b669329f57a91fc44c84fa632b9edc6
ningo247 <ningo247@users.noreply.github.com>
2023-03-04 21:58:37 +0000

    Removed unpolished tests

--- tests/test_telegram_bot.py
+++ tests/test_telegram_bot.py
@@ -3,6 +3,7 @@ import os
 import unittest.mock as mock
 from unittest.mock import MagicMock
 import telegram
+from telegram.error import BadRequest, InvalidToken, NetworkError
 import pytest
 from main import Bot
 from main import get_token
@@ -20,42 +21,48 @@ def return_telegram_token():
     token = "fake-token"
     return token
 
-class TestBot:
+def test_no_token_passed():
+    with pytest.raises(InvalidToken, match="Invalid token"):
+        Bot("")
 
-    @mock.patch('requests.get')
-    def test_start(self, mock_get, return_telegram_token):
-        mock_response = MagicMock()
-        mock_response.status_code = 200
-        mock_response.json.return_value = json.loads({"text": "Some fun fact"})
-        mock_get.return_value = mock_response
 
-        telegram_token = return_telegram_token
-        bot = Bot(telegram_token)
-        update = telegram.Update(1, message=telegram.Message(1, "/start", telegram.Chat(1,telegram.constants.CHAT_PRIVATE)))
-        context = MagicMock(args=[])
-        bot.start(update, context)
 
-        context.bot.send_message.assert_called_with(
-            chat_id=1, text='Hi, I am Senpai bot!'
-        )
-        context.bot.send_message.assert_called_with(
-            chat_id=1, text='Before we start, wanna hear a fun fact?'
-        )
-        context.bot.send_message.assert_called_with(
-            chat_id=1, text='Some fun fact'
-        )
+# class TestBot:
 
-    def test_echo(self):
-        token = get_token("TELEGRAM_TOKEN")
-        bot = Bot(token)
-        update = telegram.Update(1, message=telegram.Message(1, "/echo hello world", telegram.Chat(1,telegram.constants.CHAT_PRIVATE)))
-        context = MagicMock(args=['hello', 'world'])
-        # context.__iter__.return_value = ['hello ', 'world']
-        # context.bot.send_message.assert_called_with(chat_id=0, text="TEST MESSAGE")
-        bot.echo(update, context)
-        context.bot.send_message.assert_called_with(
-            chat_id=1, text='hello world'
-        )
+#     @mock.patch('requests.get')
+#     def test_start(self, mock_get, return_telegram_token):
+#         mock_response = MagicMock()
+#         mock_response.status_code = 200
+#         mock_response.json.return_value = json.loads({"text": "Some fun fact"})
+#         mock_get.return_value = mock_response
+
+#         telegram_token = return_telegram_token
+#         bot = Bot(telegram_token)
+#         update = telegram.Update(1, message=telegram.Message(1, "/start", telegram.Chat(1,telegram.constants.CHAT_PRIVATE)))
+#         context = MagicMock(args=[])
+#         bot.start(update, context)
+
+#         context.bot.send_message.assert_called_with(
+#             chat_id=1, text='Hi, I am Senpai bot!'
+#         )
+#         context.bot.send_message.assert_called_with(
+#             chat_id=1, text='Before we start, wanna hear a fun fact?'
+#         )
+#         context.bot.send_message.assert_called_with(
+#             chat_id=1, text='Some fun fact'
+#         )
+
+#     def test_echo(self, return_telegram_token):
+#         token = return_telegram_token
+#         bot = Bot(token)
+#         update = telegram.Update(1, message=telegram.Message(1, "/echo hello world", telegram.Chat(1,telegram.constants.CHAT_PRIVATE)))
+#         context = MagicMock(args=['hello', 'world'])
+#         # context.__iter__.return_value = ['hello ', 'world']
+#         # context.bot.send_message.assert_called_with(chat_id=0, text="TEST MESSAGE")
+#         bot.echo(update, context)
+#         context.bot.send_message.assert_called_with(
+#             chat_id=1, text='hello world'
+#         )
 
     # @mock.patch('bot.openai.Completion.create')
     # def test_unknown(self, mock_gpt):
